13b0ed5090c48149cdcf3cce46ad5c6d04d9c697
hami-dev <rinrin.xml@gmail.com>
2022-01-16 23:41:17 +0900

    Design: 헤더 간격 조정 및 font weight 수정

--- src/common/components/Header.jsx
+++ src/common/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Subtitle1, Input, Body2, Body1, Icon } from "@class101/ui";
+import { Subtitle1, Input, Body2, Body1, Icon, Colors } from "@class101/ui";
 
 function Header() {
     return (
@@ -11,8 +11,14 @@ function Header() {
                     <Subtitle1>클래스</Subtitle1>
                     <Subtitle1>스토어</Subtitle1>
                 </Page>
-                <Input placeholder="찾으시는 취미가 있으신가요?" />
-                <Page>
+                <Input
+                    placeholder="찾으시는 취미가 있으신가요?"
+                    inputStyle={{
+                        backgroundColor: Colors.gray200,
+                        borderRadius: "24px",
+                    }}
+                />
+                <Page isFlexEnd>
                     <Body2>크리에이터 지원</Body2>
                     <Body2>기업교육</Body2>
                     <Body2>로그인</Body2>
@@ -20,13 +26,13 @@ function Header() {
             </HeaderTop>
             <HeaderNav>
                 <Page>
-                    <Body1>전체 카테고리</Body1>
-                    <Body1>새해 다짐격파</Body1>
-                    <Body1>1월 가입혜택</Body1>
-                    <Body1>이벤트</Body1>
-                    <Body1>바로 수강</Body1>
-                    <Body1>신규 클래스</Body1>
-                    <Body1>오픈 예정</Body1>
+                    <Body1 fontWeight={"bold"}>전체 카테고리</Body1>
+                    <Body1 fontWeight={"bold"}>새해 다짐격파</Body1>
+                    <Body1 fontWeight={"bold"}>1월 가입혜택</Body1>
+                    <Body1 fontWeight={"bold"}>이벤트</Body1>
+                    <Body1 fontWeight={"bold"}>바로 수강</Body1>
+                    <Body1 fontWeight={"bold"}>신규 클래스</Body1>
+                    <Body1 fontWeight={"bold"}>오픈 예정</Body1>
                 </Page>
                 <Page>
                     <Body2>시그니처</Body2>
@@ -41,7 +47,8 @@ function Header() {
 const HeaderTop = styled.div`
     display: grid;
     align-items: center;
-    grid-template-columns: 1fr 1fr 2fr 1.5fr;
+    grid-template-columns: 1fr 1.5fr 3fr 3fr;
+    gap: 24px;
 `;
 
 const Logo = styled.div`
@@ -52,6 +59,11 @@ const Logo = styled.div`
 const Page = styled.div`
     display: flex;
     gap: 24px;
+    ${(props) =>
+        props.isFlexEnd &&
+        `
+    justify-content: flex-end;
+    `}
 `;
 
 const HeaderNav = styled.div`
