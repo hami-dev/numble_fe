b4e6d32dc726bc5471ffd4cc885e8735061428f5
hami-dev <rinrin.xml@gmail.com>
2022-01-16 23:02:45 +0900

    Add: 메인페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+import MainPage from "pages/MainPage";
+
 function App() {
-    return <div className="App">hello world</div>;
+    return (
+        <div className="App">
+            <MainPage />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Section } from "@class101/ui";
+
+import CarouselComponent from "common/components/Carousel";
+import Header from "common/components/Header";
+
+import todaySpecialData from "Json/todaySpecial.json";
+import mdRecommend from "Json/mdRecommend.json";
+import popularEvent from "Json/popularEvent.json";
+import openSoon from "Json/openSoon.json";
+
+function MainPage() {
+    return (
+        <MainWrapper>
+            <Header />
+            <Section title="오늘의 특가! TIME DEAL">
+                <CarouselComponent
+                    data={todaySpecialData.time_deal}
+                    cardCount="4"
+                    isTimeDeal
+                />
+            </Section>
+            <Section title="MD 추천 클래스">
+                <CarouselComponent
+                    data={mdRecommend.md_recommend}
+                    cardCount="4"
+                    isLongHeight
+                />
+            </Section>
+            <Section title="진행중인 인기 이벤트">
+                <CarouselComponent
+                    data={popularEvent.popular_event}
+                    cardCount="3"
+                    isEvent
+                />
+            </Section>
+            <Section title="오픈 예정 클래스">
+                <CarouselComponent data={openSoon.open_soon} cardCount="4" />
+            </Section>
+        </MainWrapper>
+    );
+}
+
+const MainWrapper = styled.div`
+    max-width: 1176px;
+    margin: 0 auto;
+`;
+
+export default MainPage;
